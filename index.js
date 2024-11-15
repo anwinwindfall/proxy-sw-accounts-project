5837054b71d45eaec873a4e282754ea427425c4a
anwinwindfall <anwina@windfallproductions.org>
2024-11-15 13:12:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.post('/dev-search-contact', async (req, res) => {
+    try {
+        const response = await axios.post('https://landing.shalommediastore.org/_hcms/api/accounts/search/contact', req.body);
+        res.json(response.data);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to search contact' });
+    }
+});
+
+
+
+app.get('/dev-fetch-contact-details', (req, res) => {
+    try {
+        const response = axios.get('https://landing.shalommediastore.org/_hcms/api/accounts/search/contact');
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch contact details' });
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
