381bf6d58f395cce8d5ec5f14ef6ab2e32add2fe
anwinwindfall <anwina@windfallproductions.org>
2024-11-18 12:08:01 +0530

    search contact completed

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const axios = require('axios');
 const express = require('express');
 const cors = require('cors');
+var bodyParser = require('body-parser')
 require('dotenv').config();
 const app = express();
 const PORT = 3000;
-app.use(cors());
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/dev-fetch-contact-details', async (req, res) => {
     try {
@@ -21,6 +23,22 @@ app.get('/dev-fetch-contact-details', async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to fetch contact details' });
+    }
+});
+
+app.post('/dev-search-contacts', async (req, res) => {
+
+    try {
+        const response = await axios.post('https://landing.shalommediastore.org/_hcms/api/accounts/search/contact', req.body, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + process.env.API_KEY
+            },
+        });
+        res.send(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to search contacts' });
     }
 });
 
